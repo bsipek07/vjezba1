f21f993ee0f94b11908ed78c11c156f2ea99ca50
bsipek07 <borna.sipek@gmail.com>
2025-03-17 18:52:13 +0100

    zadatak1

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int znm1 = 0;
+        int znm2 = 0;
+        int znm3 = 0;
+        int flag1 = 1;
+        int flag2 = 0;
+        for (int i = 0; i < 10000; i++) {
+            if (flag1 == 1) {
+                znm1 = i;
+                if (i == 9) {
+                    flag1 = 0;
+                    flag2 = 1;
+                }
+
+            }
+            if (flag2 == 1) {
+                znm1 = i % 10;
+                znm2 = i / 10 % 10;
+                if (i == 99) {
+                    flag2 = 2;
+                }
+
+            }
+            if (flag2 == 2) {
+                znm1 = i % 10;
+                znm2 = i / 10 % 10;
+                znm3 = i / 100 % 10;
+            }
+            if (Math.pow(znm1, 3) + Math.pow(znm2, 3) + Math.pow(znm3, 3) == i) System.out.println(i);
+
+        }
+    }
+}
